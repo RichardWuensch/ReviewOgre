a1b57e1b629f4fee42029a3dac6bd5c00a00c624
Rechberger, Jakob <jakob.rechberger@student.fhws.de>
2023-04-16 19:42:19 +0200

    header and import buttons

--- src/components/window/MainPage.js
+++ src/components/window/MainPage.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './MainPage.css';
 import logo from '../../assets/media/favicon_ogre.png'
+import download from '../../assets/media/download.svg'
+import file from '../../assets/media/file-earmark.svg'
 
 function MainPage() {
     return (
         <div className={"main-page"}>
-            <div>
-                <img src={logo} alt={"icon"}/>
-                <h1>ReviewOgre Reloaded</h1>
+            <div className={"title-box"}>
+                <img src={logo} alt={"icon"} height={100} width={100}/>
+                <span className={"title-text"}>ReviewOgre Reloaded</span>
             </div>
-            <p>Sort your review peers in groups for better Technical Reviews! Visit the HowTo Guide to learn more about this platform</p>
+            <h2 className={"title-subheadline"}>Sort your review peers in groups for better Technical Reviews!</h2>
+            <span className={"title-subheadline"} style={{fontSize:12}}>Visit the <a href="url">HowToGuide</a> to learn more about this platform</span>
+            <div className={"button-group"}>
+                <div className={"button-container-green"}>
+                    <img src={download} alt={"icon1"}/>
+                    <span className={"button-text"}>Import Configuration</span>
+                </div>
+                <div className={"button-container-green"}>
+                    <img src={download} alt={"icon2"}/>
+                    <span className={"button-text"}>Load Configuration</span>
+                </div>
+                <div className={"button-container-white"}>
+                    <img src={file} alt={"icon3"}/>
+                    <span className={"button-text"}>Save Configuration</span>
+                </div>
+            </div>
+
         </div>
     );
 }
