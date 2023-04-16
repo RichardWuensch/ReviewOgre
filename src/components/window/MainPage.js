a91b093f3501e391fd88d6e81c062a70f9a607fc
Rechberger, Jakob <jakob.rechberger@student.fhws.de>
2023-04-16 21:23:31 +0200

    finish window style IMPORTANT: participant/slots/setup windows have to be created as seperate components later on

--- src/components/window/MainPage.js
+++ src/components/window/MainPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './MainPage.css';
+import './participants_window.css'
+import './setup_window.css'
+import './slotsWindow.css'
 import logo from '../../assets/media/favicon_ogre.png'
 import download from '../../assets/media/download.svg'
 import file from '../../assets/media/file-earmark.svg'
+import add from '../../assets/media/plus-circle.svg'
+import edit from '../../assets/media/pencil-square.svg'
+import start from '../../assets/media/play-circle.svg'
 
 function MainPage() {
     return (
@@ -27,7 +33,57 @@ function MainPage() {
                     <span className={"button-text"}>Save Configuration</span>
                 </div>
             </div>
-
+            <div className={"participant-slots-container"}>
+                {/* replace with component*/}
+                <div className={"participantWindow"}>
+                    <span>Participants</span>
+                    <div className={"participant-button-container"}>
+                        <div className={"button-container-green-participants"}>
+                            <img src={add} alt={"addParticipantIcon"}/>
+                            <span className={"button-text"}>Add Participant</span>
+                        </div>
+                        <div className={"button-container-green-participants"} style={{width:100}}>
+                            <img src={edit} alt={"editListIcon"}/>
+                            <span className={"button-text"}>Edit List</span>
+                        </div>
+                    </div>
+                    <div className={"list-description"}>
+                        <span>First Name</span>
+                        <span>Last Name</span>
+                        <span>Email Address</span>
+                        <span>Group</span>
+                        <span>Options</span>
+                    </div>
+                    <div className={"participant-list-container"}>
+                    </div>
+                </div>
+                {/* end*/}
+                {/* replace with component*/}
+                <div className={"slotsWindow"}>
+                    <span>Slots</span>
+                    <div className={"slots-button-container"}>
+                        <div className={"button-container-green-slots"}>
+                            <img src={add} alt={"addParticipantIcon"}/>
+                            <span className={"button-text"}>Add Slot</span>
+                        </div>
+                    </div>
+                    <div className={"slots-list-container"}>
+                    </div>
+                </div>
+                {/* end*/}
+            </div>
+            <div className={"setup-start-container"}>
+                {/* replace with component*/}
+                <div className={"setupWindow"}>
+                    <span style={{fontFamily:"Roboto, sans-serif"}}>Setup</span>
+                    <div className={"setupContainer"}></div>
+                </div>
+                {/* end*/}
+                <div className={"button-start"}>
+                    <img src={start} alt={"startCalculationsIcon"} height={20} width={20} />
+                    <span className={"button-start-text"}>Start Calculations</span>
+                </div>
+            </div>
         </div>
     );
 }
