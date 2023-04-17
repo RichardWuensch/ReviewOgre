108c8ade6c1228eb6e8d4f111bb87420235fe400
Rechberger, Jakob <jakob.rechberger@student.fhws.de>
2023-04-17 19:55:37 +0200

    basic modal finished

--- src/components/window/addSlotRoomModal.js
+++ src/components/window/addSlotRoomModal.js
@@ -2,6 +2,8 @@ import Modal from 'react-bootstrap/Modal';
 import PropTypes from 'prop-types';
 import '../window/SlotModal.css';
 import exit from '../../../src/assets/media/x-circle.svg';
+import add from '../../assets/media/plus-circle.svg';
+
 function SlotModal (props) {
   return (
         <Modal
@@ -11,17 +13,33 @@ function SlotModal (props) {
             centered
         >
             <Modal.Body>
-                <div className={'modal-header-container'}>
-                    <h2 className={'modal-header'}>New Time Slot</h2>
-                    <img src={exit} alt={'exitSlotModal'} className={'modal-header-icon'} onClick={props.onHide}/>
-                </div>
-                <div className={'date-container'}>
-                    <input type={'date'} className={'input-date-container'}/>
-                </div>
-                <div className={'footer'}>
-                    <button className={'add-slot-button'}>
-                        <span className={'add-slot-text'}>Add Slot</span>
-                    </button>
+                <div className={'modal-container'}>
+                    <div className={'modal-header-container'}>
+                        <h2 className={'modal-header'}>New Time Slot</h2>
+                        <img src={exit} alt={'exitSlotModal'} className={'modal-header-icon'} onClick={props.onHide}/>
+                    </div>
+                    <div className={'date-container'}>
+                        <input type={'date'} className={'input-date-container'}/>
+                    </div>
+                    <div className={'time-container'}>
+                        <span>From:</span>
+                        <input className={'input-time-container'}/>
+                        <span>To:</span>
+                        <input className={'input-time-container'}/>
+                    </div>
+                    <div className={'room-container'}>
+                        <span>Create Rooms for this Time Slot:</span>
+                        <div>
+                            <button className={'add-room-button'}>
+                                <img src={add} alt={'addRoomIcon'}/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={'footer'}>
+                        <button className={'add-slot-button'}>
+                            <span className={'add-slot-text'}>Add Slot</span>
+                        </button>
+                    </div>
                 </div>
             </Modal.Body>
         </Modal>
