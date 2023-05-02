921a08372b7bc17e9596bcaeccd0d8620ba5cdd4
Wünsch, Richard <richard.wuensch@student.fhws.de>
2023-05-02 11:34:37 +0200

    class for some frequently used string conversions

---
+++ src/api/ConverterForPrinting.js
@@ -0,0 +1,32 @@
+export default class ConverterForPrinting {
+  /**
+  * Get the Date from the Date()
+  * @param {date} date
+  * @returns {string} - in format 'dd.mm.yyyy'
+  */
+  getDataDDmmYYYYforPrinting (date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    return ((day < 10) ? ('0' + day) : day) + '.' + ((month < 10) ? ('0' + month) : month) + '.' + date.getFullYear();
+  }
+
+  /**
+  * Get the Time from the Date()
+  * @param {date} time
+  * @returns {string} - in format 'HH:MM'
+  */
+  getTimeHHmm (time) {
+    const hour = time.getHours();
+    const min = time.getMinutes();
+    return ((hour < 10) ? ('0' + hour) : hour) + ':' + ((min < 10) ? ('0' + min) : min) + 'Uhr';
+  }
+
+  /**
+  * Makes the participant to a simple string with the nessecary attributes
+  * @param {participant} participant
+  * @returns {string} - in format 'FirstName LastName Email Group group'
+  */
+  getParticipantAttributsForPrinting (participant) {
+    return participant.getFirstName() + ' ' + participant.getLastName() + ' ' + participant.getEmail() + ' Gruppe ' + participant.getGroup();
+  }
+}
