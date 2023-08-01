2c3ca428ad6fa4419fe406bcc554d4ee72ba71df
Wünsch, Richard <richard.wuensch@student.fhws.de>
2023-08-01 21:21:05 +0200

    add log to show possibleParticipants

--- src/algorithm/logic/Runner.js
+++ src/algorithm/logic/Runner.js
@@ -4,5 +4,9 @@ export default class Runner {
   runAlgorithm (participants, participantsDispatch, roomSlots, roomSlotsDispatch, settings) {
     const algo = new Algorithm(participants, participantsDispatch, roomSlots, roomSlotsDispatch, settings);
     algo.run();
+
+    // only to see which participant is possible for testing dragndrop
+    const result = algo.getResult();
+    console.log(result[0].getRooms()[0].getReview().getPossibleParticipants());
   }
 }
