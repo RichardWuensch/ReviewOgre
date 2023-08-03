177c8b6b61f33306e6e9598654017cd8dc2b9eda
Stoll, Nicolas <nicolas.stoll@study.thws.de>
2023-08-03 19:00:49 +0200

    - Fix: SVG now displayed again

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(() => {
     return {
         build: {
             outDir: 'build',
         },
-        plugins: [react()],
+        plugins: [
+            react(),
+            svgr({ svgrOptions: { icon: true } }),
+        ],
     };
 });
