c58697997c0a8b9a00b50fe5317e04b21528cdf0
Wünsch, Richard <richard.wuensch@student.fhws.de>
2023-08-12 07:58:22 +0200

    mergeconflict

--- src/components/resultPage/ResultCalculationPage.jsx
+++ src/components/resultPage/ResultCalculationPage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useRoomSlots } from '../shared/context/RoomSlotContext';
 // import ReviewWindowV1 from './reviewWindowV1/ReviewWindowV1';
 import ReviewWindow from './reviewWindow/ReviewWindow';
-import {HashLink} from "react-router-hash-link";
+// import {HashLink} from "react-router-hash-link";
 
 function ResultCalculationPage () {
   const roomSlots = useRoomSlots();
@@ -19,7 +19,7 @@ function ResultCalculationPage () {
       <div className="review-page">
           {hasReviewResults()
             ? <Container>
-                  <ReviewWindowV1 />
+                  <ReviewWindow />
               </Container>
             : <Container className="d-flex justify-content-center align-items-center">
                 <h3>No Reviews calculated yet</h3>
