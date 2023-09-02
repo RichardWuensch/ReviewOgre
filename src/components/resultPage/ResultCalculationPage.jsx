7aad49780f885466698ffd2cc6e41bca3685a208
Stoll, Nicolas <nicolas.stoll@study.thws.de>
2023-09-02 11:49:28 +0200

    - Added: Further Infos to readme

--- src/components/resultPage/ResultCalculationPage.jsx
+++ src/components/resultPage/ResultCalculationPage.jsx
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import React from 'react';
 import { useRoomSlots } from '../shared/context/RoomSlotContext';
 import ReviewWindowV1 from './reviewWindowV1/ReviewWindowV1';
+import ReviewWindow from "./reviewWindow/ReviewWindow";
 // import ReviewWindow from './reviewWindow/ReviewWindow';
 
 function ResultCalculationPage () {
@@ -18,7 +19,7 @@ function ResultCalculationPage () {
       <div className="review-page">
           {hasReviewResults()
             ? <Container>
-                  <ReviewWindowV1 />
+                  <ReviewWindow />
               </Container>
             : <Container className="d-flex justify-content-center align-items-center">
                 <h3>No Reviews calculated yet</h3>
